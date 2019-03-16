c3e80c5080265998c87e3ca347d6eb30ea52b555
John Carey <jcarey1973@gmail.com>
2019-03-16 11:17:55 -0400

    Add Countdown timer to landing page
    Countdown to the next event

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
+import  Countdown from './Countdown';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    var endDate = new Date(new Date().getTime() + 30000);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Countdown date={endDate}> </Countdown>
+
         </header>
       </div>
     );
