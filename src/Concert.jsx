d2a75abb0069813939c22a54da735c1100e25ffc
John Carey <jcarey1973@gmail.com>
2021-11-04 20:16:04 -0400

    youtube link in concert page

--- src/Concert.jsx
+++ src/Concert.jsx
@@ -2,11 +2,13 @@ import React, { useContext, useState } from "react";
 
 import UserContext from "./UserContext";
 import useInterval from "./interval.js";
+import Countdown from "./Countdown";
 
 function Concert(props) {
   const [time, setTime] = useState(new Date().getTime());
   const [delay, setDelay] = useState(1000);
   const user = useContext(UserContext);
+  const endDate = new Date("Nov 06 2021 21:00:00 GMT-0400");
 
   useInterval(() => {
     var newTime = new Date().getTime();
@@ -25,6 +27,8 @@ function Concert(props) {
   return (
     <div className={className}>
       <p> Get ready to Rock!!</p> <p> Be ready to leave by 9:00 PM </p>
+      <Countdown endDate={endDate} />
+      <a href="https://youtu.be/B9mv3NmKPsg">https://youtu.be/B9mv3NmKPsg</a>
     </div>
   );
 }
